9df99a2c39a14e9d3034b7476aaac146ada1b290
PedroPablo <pedroPVP0516@gmail.com>
2021-08-12 14:55:52 -0600

    Validición realizada para clientes y productos en cada una de sus funciones.

--- Cliente/ClienteServices.cs
+++ Cliente/ClienteServices.cs
@@ -11,6 +11,14 @@ namespace Taller_POO.Cliente
         {
             Console.WriteLine("digite el documento del cliente");
             string documento = Console.ReadLine();
+            //Validación Clientes
+            var verClientes = ListaClientes.Any(c => c.Documento == documento);
+           while(verClientes){
+                    Console.WriteLine("El documento ya existe, por favor digite el nuevo documento:");
+                    documento = Console.ReadLine();
+                    verClientes = ListaClientes.Any(c => c.Documento == documento);
+            }
+
             Console.WriteLine("digite el nombre del cliente");
             string nombre = Console.ReadLine();
             Console.WriteLine("digite el direccion del cliente");
@@ -27,19 +35,39 @@ namespace Taller_POO.Cliente
         {
             Console.WriteLine("digite la cédula del cliente");
             string documento = Console.ReadLine();
+            //Validación Clientes
+            var verClientes = ListaClientes.Any(c => c.Documento == documento);
+            if (verClientes){
+                var cliente = ListaClientes.Where(c => c.Documento == documento).FirstOrDefault();
+                Console.WriteLine("datos del cliente consultado\n");
+                Console.WriteLine($"documento: {cliente.Documento}\nnombre: {cliente.Nombre}\ndireccion: {cliente.Direccion}\ntelefono: {cliente.Telefono} \n");
+            }else{
+                while(!verClientes){
+                    Console.WriteLine("El Documento NO ha sido encontrado, digite de nuevo");
+                    documento = Console.ReadLine();
 
-            //var consulta = (from cliente in ListaClientes where cliente.documento.Equals(documento) select cliente).FirstOrDefault();
-            var cliente = ListaClientes.Where(c => c.Documento == documento).FirstOrDefault();
-            Console.WriteLine("datos del cliente consultado\n");
-            Console.WriteLine($"documento: {cliente.Documento}\nnombre: {cliente.Nombre}\ndireccion: {cliente.Direccion}\ntelefono: {cliente.Telefono} \n");
+                    verClientes = ListaClientes.Any(c => c.Documento == documento);
 
+                    if (verClientes){
+                        var cliente = ListaClientes.Where(c => c.Documento == documento).FirstOrDefault();
+                        Console.WriteLine("datos del cliente consultado\n");
+                        Console.WriteLine($"documento: {cliente.Documento}\nnombre: {cliente.Nombre}\ndireccion: {cliente.Direccion}\ntelefono: {cliente.Telefono} \n");        
+                    }
+
+                }
+            }
         }
         public void EditarCliente(List<Cliente> ListaClientes)
         {
             Console.WriteLine("digite la cédula del cliente");
             string documento = Console.ReadLine();
-
-            //var consulta = (from cliente in ListaClientes where cliente.documento.Equals(documento) select cliente).FirstOrDefault();
+            //Validación Cliente
+            var verClientes = ListaClientes.Any(c => c.Documento == documento);
+           while(!verClientes){
+                Console.WriteLine("El documento NO  existe, por favor digite el nuevo documento:");
+                documento = Console.ReadLine();
+                verClientes = ListaClientes.Any(c => c.Documento == documento);
+            }
             var cliente = ListaClientes.Where(c => c.Documento == documento).FirstOrDefault();
             Console.WriteLine("datos del cliente \n");
             Console.WriteLine($"documento: {cliente.Documento}\nnombre: {cliente.Nombre}\ndireccion: {cliente.Direccion}\ntelefono: {cliente.Telefono} \n");
@@ -61,13 +89,19 @@ namespace Taller_POO.Cliente
         {
             Console.WriteLine("digite la cédula del cliente");
             string documento = Console.ReadLine();
-
-            //var consulta = (from cliente in ListaClientes where cliente.documento.Equals(documento) select cliente).FirstOrDefault();
+            //Validación Cliente
+            var verClientes = ListaClientes.Any(c => c.Documento == documento);
+           while(!verClientes){
+                Console.WriteLine("El documento NO existe, por favor digite el nuevo documento:");
+                documento = Console.ReadLine();
+                verClientes = ListaClientes.Any(c => c.Documento == documento);
+            }
             var cliente = ListaClientes.Where(c => c.Documento == documento).FirstOrDefault();
             Console.WriteLine("datos del cliente \n");
             Console.WriteLine($"documento: {cliente.Documento}\nnombre: {cliente.Nombre}\ndireccion: {cliente.Direccion}\ntelefono: {cliente.Telefono} \n");
             ListaClientes.Remove(cliente);
             Console.WriteLine("cliente eliminado satisfactoriamente");
         }
+
     }
 }
